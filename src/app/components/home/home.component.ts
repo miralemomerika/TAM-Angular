5b7a26bc009f9eca8c2faa5f842873d081e78d59
Tarik Čolakhodžić <tarik.colakhodzic@edu.fit.ba>
2021-05-17 22:44:54 +0200

    Najpopularniji kursevi added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,8 @@
 import { StatistikaService } from './../../core/services/statistika.service';
 import { Statistika } from './../../core/models/Statistika';
 import { Component, OnInit } from '@angular/core';
+import { Kurs } from 'src/app/core/models/Kurs';
+import { KursService } from 'src/app/core/services/kurs.service';
 
 @Component({
   selector: 'app-home',
@@ -10,10 +12,14 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   statistika!: Statistika;
+  public kursevi!: Kurs[];
 
-  constructor(private statistikaService: StatistikaService) {
+  constructor(private statistikaService: StatistikaService, private kursService: KursService) {
     this.statistikaService.getStatistika().subscribe(x => {
       this.statistika = x;
+    });
+    this.kursService.getNajpopularnije().subscribe(x => {
+      this.kursevi = x;
     });
    }
 
